fdf3bb22c4bb280085d4d42ba3460c7210721ddf
Yunus <yunuskhansia@yahoo.co.uk>
2024-10-20 15:14:38 +0100

    Update main.py

--- PythonProjects/Introduction to Python/100 Days of Code - The Complete Python Pro Bootcamp/Day 9/Blind Auction Project/main.py
+++ PythonProjects/Introduction to Python/100 Days of Code - The Complete Python Pro Bootcamp/Day 9/Blind Auction Project/main.py
@@ -1,6 +1,59 @@
-# TODO-1: Ask the user for input
+# TODO-1: Ask the user for inputt
+import art
+print(art.logo)
+
+more_bidders = "yes"
+auction = {}
+while more_bidders == "yes":
+    auction.update({input("What is your name?: "): int(input("What is your bid?: $"))})
+    more_bidders = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    print("\n" * 100)
+else:
+    maximum = 0
+    for key in auction:
+        if auction[key] > maximum:
+            maximum = auction[key]
+    for key in auction:
+        if maximum == auction[key]:
+            print(f"The winner is {key} with a bid of ${maximum}")
+
+
+
+
+
+
+#def add_bidders(bidders, name = name, bid = bid):
+#    new_bidders = {}
+#    for key in bidders:
+#        new_bidders[name] = bid
+#    return new_bidders
+#    add_bidders(name,bid)
+#stop_bidding = False
+
+
+#auction = {}
+#def start_biding():
+#    stop_bidding = False
+#    while not stop_bidding:
+#        auction.update({input("What is your name?: "):int(input("What is your bid?: $"))})
+#        print(auction)
+    #print("\n" * 100)
+    #print(auction)
+
 # TODO-2: Save data into dictionary {name: price}
+#start_biding()
 # TODO-3: Whether if new bids need to be added
+#more_bidders = input("Are there any other bidders? Type 'yes or 'no'.\n")
+#if more_bidders == "yes":
+#    start_biding()
+#else:
+#    stop_bidding = True
+#while more_bidders == "yes":
+    #more_bidders = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    #print("\n" * 100)
+ #   start_biding()
+
 # TODO-4: Compare bids in dictionary
+# The winner is h with a bid of $6
 
 
